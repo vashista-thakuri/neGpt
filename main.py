d8c17179ffe13001711c5254a76867f5deb2451e
Thakuri, Vashista <vashista.thakuri@cotiviti.com>
2025-04-30 22:09:30 +0530

    Added Training Scripts

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from Data.cleaning import NepaliTextCleaner
+from Model.modelTraining import ModelTrainer
 
 if __name__ == "__main__":
-    parent_folder = "/path/to/your/parent/directory"  # Replace with your actual path
+
+    # Data Prep
+    parent_folder = "C:/Users/vashista.thakuri/Downloads/archive/nepali_news_dataset_20_categories_large/nepali_news_dataset_20_categories_large"
 
     ntc = NepaliTextCleaner(parent_folder)
     clean_data = ntc.process_and_clean()
 
     print("Cleaned text is ready for further processing.")
+
+    # Model Training
+    trainer = ModelTrainer(clean_data)
+    trainer.train()
+
+    # Generate text using the trained model
+    generated_text = trainer.generate_text()
+    print(generated_text)
